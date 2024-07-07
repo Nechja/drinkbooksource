46e28a3499c1aa358ec803a78efa6ad3a1da5d55
kayla graves <kayla.graves@gmail.com>
2024-07-07 14:28:29 -0700

    updated bookley

--- Scripts/LayoutBuilder.py
+++ Scripts/LayoutBuilder.py
@@ -10,11 +10,10 @@ import string
 class DrinkFormatter:
     def __init__(self):
         self.styles = getSampleStyleSheet()
-        self.title_style = ParagraphStyle(name='Title', parent=self.styles['Heading2'], fontSize=8, leading=10)
+        self.title_style = ParagraphStyle(name='Title', parent=self.styles['Heading1'], fontSize=9, leading=9)
         self.subtitle_style = ParagraphStyle(name='Subtitle', parent=self.styles['Normal'], fontSize=6, leading=7)
         self.normal_style = ParagraphStyle(name='Normal', parent=self.styles['Normal'], fontSize=5, leading=10)
         self.letter_style = ParagraphStyle(name='Letter', parent=self.styles['Heading1'], fontSize=14, leading=10)
-
 
     def format_instructions(self, instructions):
         formatted_instructions = []
@@ -43,6 +42,7 @@ class DrinkFormatter:
 
     def format_drink_details(self, drink):
         drink_content = []
+        drink_content.append(HRFlowable(width="100%", thickness=.5, color="black", spaceBefore=1, spaceAfter=1))
         drink_content.extend(self.format_drink_title(drink['Name']))
 
         details_text = f"<b>Glass:</b> {drink['Glass']}"
@@ -53,20 +53,22 @@ class DrinkFormatter:
             details_text += f" <b>Garnish:</b> {garnishes}"
 
         drink_content.append(Paragraph(details_text, self.normal_style))
-        drink_content.append(Spacer(1, 4))
+        drink_content.append(Spacer(1, 3))
 
         instructions = self.format_instructions(drink['Instructions'])
         for instruction in instructions:
             drink_content.append(Paragraph(instruction, self.normal_style))
 
         drink_content.append(Spacer(1, 8))
-        drink_content.append(HRFlowable(width="100%", thickness=.5, color="black", spaceBefore=1, spaceAfter=1))
+        
 
         return KeepTogether(drink_content)
 
 class DocumentTemplate:
-    def __init__(self, doc):
+    def __init__(self, doc, page_width, page_height):
         self.doc = doc
+        self.page_width = page_width
+        self.page_height = page_height
 
     def create_two_column_template(self):
         frame_margin = 0.01 * inch 
@@ -78,11 +80,12 @@ class DocumentTemplate:
         self.doc.addPageTemplates([PageTemplate(id='TwoCol', frames=[frame1, frame2], onPage=self.add_page_number)])
 
     def add_page_number(self, canvas, doc):
-        canvas.saveState()
-        page_number_text = f"Page {doc.page}"
-        canvas.setFont('Helvetica', 8)
-        canvas.drawCentredString(4.25 * inch, 0.5 * inch, page_number_text)
-        canvas.restoreState()
+        if doc.page > 1:  
+            canvas.saveState()
+            page_number_text = f"Page {doc.page - 1}"  
+            canvas.setFont('Helvetica', 8)
+            canvas.drawCentredString(self.page_width / 2, 0.5 * inch, page_number_text)
+            canvas.restoreState()
 
 class DrinkDataLoader:
     @staticmethod
@@ -92,16 +95,19 @@ class DrinkDataLoader:
         return sorted(drinks, key=lambda d: d['Name'].upper())
 
 class PDFDocumentBuilder:
-    def __init__(self, filepath):
+    def __init__(self, filepath, page_width=5.5 * inch, page_height=8.5 * inch):
         self.filepath = filepath
-        self.doc = BaseDocTemplate("drink_booklet.pdf", pagesize=portrait((5.5 * inch, 8.5 * inch)))
+        self.page_width = page_width
+        self.page_height = page_height
+        self.doc = BaseDocTemplate("drink_booklet.pdf", pagesize=portrait((self.page_width, self.page_height)))
         self.formatter = DrinkFormatter()
         self.current_letter_group = ''
-        self.document_template = DocumentTemplate(self.doc)
+        self.document_template = DocumentTemplate(self.doc, self.page_width, self.page_height)
         self.document_template.create_two_column_template()
 
     def build_document(self):
         content = []
+        self.add_title_page(content)
         drinks = DrinkDataLoader.load_drinks(self.filepath)
         current_letter = ''
         for drink in drinks:
@@ -116,6 +122,14 @@ class PDFDocumentBuilder:
             content.append(drink_details)
         self.doc.build(content)
 
+    def add_title_page(self, content):
+        title = "AFK Drink Book"
+        title_paragraph = Paragraph(title, self.formatter.title_style)
+        content.append(Spacer(1, 2 * inch))
+        content.append(HRFlowable(width="80%", thickness=.5, color="black", spaceBefore=1, spaceAfter=1, hAlign="LEFT"))
+        content.append(title_paragraph)
+        
+
     def add_letter_group(self, letter, content):
         content.append(PageBreak())
         header = Paragraph(f"<b>{letter}</b>", self.formatter.letter_style)
@@ -125,5 +139,5 @@ class PDFDocumentBuilder:
         content.append(Spacer(1, 12))
 
 if __name__ == "__main__":
-    pdf_builder = PDFDocumentBuilder(os.path.abspath(os.path.join('Drinks', 'source.json')))
+    pdf_builder = PDFDocumentBuilder(os.path.abspath(os.path.join('Drinks', 'source.json')), page_width=6 * inch, page_height=9 * inch)
     pdf_builder.build_document()
